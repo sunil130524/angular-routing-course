bae5177ead3a90ad73dba8fb8ca3cf4a94cf385e
Sunil Kumar C <sunil130524@gmail.com>
2024-10-04 11:54:05 +0530

    Hooking into router events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private readonly router: Router) {
+
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      // Executed after Router Event: NavigationStart
+      if(event instanceof NavigationStart) {
+        console.log("App Component Navigation Start");
+      }
+    })
+  }
+
+  // Executed after Router Event: ResolveEnd
+  onActivate(event: unknown) {
+    console.log("App Component: On Activate method");
+  }
+
 }
