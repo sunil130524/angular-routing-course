29ebce4ea84c53a1ad17f5ea88645624737db70c
Sunil Kumar C <sunil130524@gmail.com>
2024-10-04 19:54:46 +0530

    Routing from Class

--- src/app/home/sub-section/card/card.component.ts
+++ src/app/home/sub-section/card/card.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ROUTER_TOKENS } from 'src/app/app.routes';
 import { Pie } from 'src/app/models/pie';
 import { CartService } from 'src/app/services/cart.service';
+import { PieService } from 'src/app/services/pie.service';
 
 @Component({
   standalone: true,
@@ -14,5 +17,17 @@ import { CartService } from 'src/app/services/cart.service';
 })
 export class CardComponent {
   @Input() pie!: Pie;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private pieService: PieService) {
+
+  }
+
   readonly cartService = inject(CartService);
+
+  selectPie(pie: Pie) {
+    this.pieService.setSelectedPie(pie.id);
+    this.router.navigate([`../${ROUTER_TOKENS.SHOP}`], {
+      relativeTo: this.activatedRoute
+    })
+  }
 }
